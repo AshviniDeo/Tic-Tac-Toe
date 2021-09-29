36556a65a28ac44765c096c7831dc9be9121efed
Ashvini Deokate <89019792+AshviniDeo@users.noreply.github.com>
2021-09-30 00:06:58 +0530

    Player choose symbol

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,11 +1,22 @@
 package bridgeLabz;
 
+import java.util.Scanner;
+
 public class TicTacToe {
 
 	public static void main(String[] args) {
 		System.out.println("::Welcome to Tic-Tac-Toe board::");
 		displayBoard(createBoard());
+		chooseSymbol();
 
+	}
+	private static char chooseSymbol() {
+		System.out.println("Choose your Symbol to Play X or O :");
+		Scanner scanner = new Scanner(System.in);
+		char symbol = scanner.next().toUpperCase().charAt(0);
+		char player = (symbol !='X')?'O':'X';
+		return player;
+		
 	}
 	private static char[] displayBoard(char[] board) {
 		System.out.println(" "+ board[1] + "  | " + board[2] + " | " + board[3]);
